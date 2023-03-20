38b236d6b6cb0d14466118c5a89c6d5d5abd31d3
fastaqim96 <86286565+fastaqim96@users.noreply.github.com>
2023-03-21 00:25:47 +0800

    changes studentCreate.php

--- application/views/student/studentCreate.php
+++ application/views/student/studentCreate.php
@@ -46,8 +46,9 @@
                                         <?php if ($sch_setting->roll_no) { ?>
                                             <div class="col-md-3">
                                                 <div class="form-group">
-                                                    <label for="exampleInputEmail1"><?php echo $this->lang->line('roll_no'); ?></label>
-                                                    <input id="roll_no" name="roll_no" placeholder="" type="text" class="form-control"  value="<?php echo set_value('roll_no'); ?>" />
+                                                    <label for="exampleInputEmail1"><?php echo $this->lang->line('roll_no'); ?></label> <small class="req"> *</small>
+
+                                                    <input autofocus="" id="roll_no" name="roll_no" placeholder="" type="text" class="form-control"  value="<?php echo set_value('roll_no'); ?>" />
                                                     <span class="text-danger"><?php echo form_error('roll_no'); ?></span>
                                                 </div>
                                             </div>
